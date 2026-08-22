6b0865b3267af04e7cf62dc4416483321f47a957
crazy8182 <crazygaming4694@gmail.com>
2026-08-22 14:03:02 +0530

    Update home function to copy message and edit text

--- bot/handlers/user.py
+++ bot/handlers/user.py
@@ -252,7 +252,8 @@ async def check(update, context):
 async def home(update, context):
     q = update.callback_query
     await q.answer()
-    await safe_edit_message(q.message, '👋 Welcome\n\n https://t.me/akimaxmovieshub/92 \n\nHelp k liye uper ki video dekhein.\n\nPremium lene ke liye pehle Buy Premium pe tap karein.\n\nAgar payment already ho gaya hai, toh Buy Premium me category select karke Paid button pe tap karke apna payment proof submit karein.', reply_markup=main_menu())
+    await context.bot.copy_message(chat_id=update.effective_chat.id,from_chat_id="@akimaxmovieshub",message_id=92)
+    await safe_edit_message(q.message, '👋 Welcome\n\nHelp k liye uper ki video dekhein.\n\nPremium lene ke liye pehle Buy Premium pe tap karein.\n\nAgar payment already ho gaya hai, toh Buy Premium me category select karke Paid button pe tap karke apna payment proof submit karein.', reply_markup=main_menu())
 
 async def help_cb(update, context):
     q = update.callback_query
