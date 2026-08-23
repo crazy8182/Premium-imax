4695e738fa70f9edf5247360a429691fb7c4711d
crazy8182 <crazygaming4694@gmail.com>
2026-08-23 11:33:56 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -51,10 +51,10 @@ def _plan_offer(index):
     }
 
 PLANS = [
-    {"id":"p1","name":os.getenv("PLAN_1_NAME","1 Month"),"days":int(os.getenv("PLAN_1_DAYS","30")),"price":int(os.getenv("PLAN_1_PRICE","99")),"offer":_plan_offer(1)},
-    {"id":"p2","name":os.getenv("PLAN_2_NAME","3 Months"),"days":int(os.getenv("PLAN_2_DAYS","90")),"price":int(os.getenv("PLAN_2_PRICE","249")),"offer":_plan_offer(2)},
-    {"id":"p3","name":os.getenv("PLAN_3_NAME","6 Months"),"days":int(os.getenv("PLAN_3_DAYS","180")),"price":int(os.getenv("PLAN_3_PRICE","449")),"offer":_plan_offer(3)},
-    {"id":"p4","name":os.getenv("PLAN_4_NAME","1 Year"),"days":int(os.getenv("PLAN_4_DAYS","365")),"price":int(os.getenv("PLAN_4_PRICE","799")),"offer":_plan_offer(4)},
+    {"id":"p1","name":os.getenv("PLAN_1_NAME","SILVER"),"days":int(os.getenv("PLAN_1_DAYS","30")),"price":int(os.getenv("PLAN_1_PRICE","99")),"offer":_plan_offer(1)},
+    {"id":"p2","name":os.getenv("PLAN_2_NAME","GOLD"),"days":int(os.getenv("PLAN_2_DAYS","90")),"price":int(os.getenv("PLAN_2_PRICE","249")),"offer":_plan_offer(2)},
+    {"id":"p3","name":os.getenv("PLAN_3_NAME","PLATINUM"),"days":int(os.getenv("PLAN_3_DAYS","180")),"price":int(os.getenv("PLAN_3_PRICE","449")),"offer":_plan_offer(3)},
+    {"id":"p4","name":os.getenv("PLAN_4_NAME","DAIMOND"),"days":int(os.getenv("PLAN_4_DAYS","365")),"price":int(os.getenv("PLAN_4_PRICE","799")),"offer":_plan_offer(4)},
 ]
 PLAN_MAP = {p["id"]: p for p in PLANS}
 
